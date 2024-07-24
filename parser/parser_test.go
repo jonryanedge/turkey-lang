ce1f75e87f397c4ab140d30f733d20ddf9e3d007
Jon Edgeworth <jedgeworth@echofive.net>
2024-07-24 10:16:12 -0400

    +parse let statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,7 +6,7 @@ import (
 	"turkey-lang/lexer"
 )
 
-func TestLetStatement(t *testing.T) {
+func TestLetStatements(t *testing.T) {
 	input := `
   let x = 5;
   let y = 10;
@@ -17,6 +17,7 @@ func TestLetStatement(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -64,4 +65,17 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
