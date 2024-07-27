c0ca994485cd4b78c88e47b09612d1638c284262
Jon Edgeworth <jedgeworth@echofive.net>
2024-07-27 06:54:02 -0400

    RPPL done

--- repl/repl.go
+++ repl/repl.go
@@ -5,10 +5,26 @@ import (
 	"fmt"
 	"io"
 	"turkey-lang/lexer"
-	"turkey-lang/token"
+	"turkey-lang/parser"
 )
 
 const PROMPT = ">> "
+
+const TURKEY_ART = `
+            .--.
+           /} p \             /}
+          '~)-) /           /' }
+           ( / /          /'}.' }
+            / / .-'""-.  / ' }-'}
+           / (.'       \/ '.'}_.}
+          |            '}   .}._}
+          |     .-=-';   } ' }_.}
+          \    '.-=-;'  } '.}.-}
+           '.   -=-'    ;,}._.}
+             '-,_  __.'' '-._}
+                 '|||
+                .=='=,
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +38,29 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	Logo(out)
+	io.WriteString(out, "Gobble Gobble! Got some dressing to do!\n")
+	io.WriteString(out, " parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+func Logo(out io.Writer) {
+	io.WriteString(out, TURKEY_ART)
+	io.WriteString(out, "\n\n")
 }
