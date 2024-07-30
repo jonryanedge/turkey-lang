06572e770aeda4dc4fcf8c7f0f7c30175ab02763
Jon Edgeworth <jedgeworth@echofive.net>
2024-07-30 13:16:30 -0400

    peekToken(;) fix

--- parser/parser.go
+++ parser/parser.go
@@ -330,7 +330,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	stmt.ReturnValue = p.parseExpression(LOWEST)
 
-	for p.curTokenIs(token.SEMICOLON) {
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -354,7 +354,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for p.curTokenIs(token.SEMICOLON) {
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
