c23f94416e1cd94a848c2f66736ef7312d448ad1
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-02 10:07:00 -0400

    +puts builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "turkey-lang/object"
+import (
+	"fmt"
+	"turkey-lang/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -92,6 +95,15 @@ var builtins = map[string]*object.Builtin{
 
 			return &object.Array{Elements: newElements}
 
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
