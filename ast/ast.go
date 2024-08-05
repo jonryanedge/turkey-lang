a5ebb96581da22b5e54a0a74ceaaa11791416aec
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-05 14:56:40 -0400

    +macro literal

--- ast/ast.go
+++ ast/ast.go
@@ -21,6 +21,31 @@ type Expression interface {
 	expressionNode()
 }
 
+type MacroLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (ml *MacroLiteral) expressionNode()      {}
+func (ml *MacroLiteral) TokenLiteral() string { return ml.Token.Literal }
+func (ml *MacroLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range ml.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(ml.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(ml.Body.String())
+
+	return out.String()
+}
+
 type HashLiteral struct {
 	Token token.Token
 	Pairs map[Expression]Expression
