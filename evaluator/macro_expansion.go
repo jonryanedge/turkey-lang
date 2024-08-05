0196d400ff0170354488c071470d413fef561f6f
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-05 16:30:15 -0400

    +expand macros & test

--- evaluator/macro_expansion.go
+++ evaluator/macro_expansion.go
@@ -5,6 +5,32 @@ import (
 	"turkey-lang/object"
 )
 
+func ExpandMacros(program ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		callExpression, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+
+		macro, ok := isMacroCall(callExpression, env)
+		if !ok {
+			return node
+		}
+
+		args := showArgs(callExpression)
+		evalEnv := extendMacroEnv(macro, args)
+
+		evaluated := Eval(macro.Body, evalEnv)
+
+		show, ok := evaluated.(*object.Show)
+		if !ok {
+			panic("we only support returning AST-nodes from macros")
+		}
+
+		return show.Node
+	})
+}
+
 func DefineMacros(program *ast.Program, env *object.Environment) {
 	definitions := []int{}
 
@@ -24,6 +50,25 @@ func DefineMacros(program *ast.Program, env *object.Environment) {
 	}
 }
 
+func isMacroCall(exp *ast.CallExpression, env *object.Environment) (*object.Macro, bool) {
+	identifier, ok := exp.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(identifier.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		return nil, false
+	}
+
+	return macro, true
+}
+
 func isMacroDefinition(node ast.Statement) bool {
 	letStatement, ok := node.(*ast.LetStatement)
 	if !ok {
@@ -36,6 +81,26 @@ func isMacroDefinition(node ast.Statement) bool {
 	}
 
 	return true
+}
+
+func showArgs(exp *ast.CallExpression) []*object.Show {
+	args := []*object.Show{}
+
+	for _, a := range exp.Arguments {
+		args = append(args, &object.Show{Node: a})
+	}
+
+	return args
+}
+
+func extendMacroEnv(macro *object.Macro, args []*object.Show) *object.Environment {
+	extended := object.NewEnclosedEnvironment(macro.Env)
+
+	for paramIdx, param := range macro.Parameters {
+		extended.Set(param.Value, args[paramIdx])
+	}
+
+	return extended
 }
 
 func addMacro(stmt ast.Statement, env *object.Environment) {
